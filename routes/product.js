d482ad83f9d2b923057a54019af397f5a19ae730
Acolytetech <slawaniya03@gmail.com>
2024-08-13 16:32:18 +0530

    ....

--- routes/product.js
+++ routes/product.js
@@ -25,28 +25,12 @@ const upload = multer({ storage: storage });
 
 // Get all products
 router.get('/', getAllProducts);
-router.get('/:id', async (req, res) => {
-  console.log('Request ID:', req.params.id); // Debugging line
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ message: 'Invalid ID format' });
-  }
 
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    console.error('Error fetching product by ID:', error);
-    res.status(500).json({ message: 'Server error', error: error.message });
-  }
-});
 // Get a single product by ID
 router.get('/:orderNumber', getProductById);
 
+router.get('/:productId', getProductById);
 router.put('/:id', upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'additionalImages', maxCount: 10 }]), updateProduct);
-
 
 // Create a new product with image upload
 router.post('/', upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'additionalImages', maxCount: 10 }]), createProduct);
